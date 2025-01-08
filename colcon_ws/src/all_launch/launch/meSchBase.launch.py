15110eb07957394d27f79d01c5d83328e5855167
kalebbennaveed <kalebbennaveed@gmail.com>
2025-01-08 18:11:38 -0500

    meSch_base_fixed

---
+++ colcon_ws/src/all_launch/launch/meSchBase.launch.py
@@ -0,0 +1,26 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+
+def generate_launch_description():
+
+    # launch the base meSch 
+    meSch_base_node = Node(
+        name="meSch_base_node",
+        executable="meSch_central_node",
+        package="meSch_base"
+        )
+
+    nodes = [
+            meSch_base_node
+            ]
+
+    return LaunchDescription(nodes)
